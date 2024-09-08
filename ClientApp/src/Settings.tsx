37bf91e5693ef890ec29fe697721e5f52f15c39b
mtbennett62 <mtbennett62@googlemail.com>
2024-09-08 21:24:06 +0100

    Add routing and tabs to settings page

--- ClientApp/src/Settings.tsx
+++ ClientApp/src/Settings.tsx
@@ -1,3 +1,4 @@
+import { Tabs } from "@radix-ui/themes";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -6,7 +7,20 @@ function Settings() {
     <div>
       <h1>Settings</h1>
 
-      <PrioritySettings />
+      <Tabs.Root defaultValue="categories">
+        <Tabs.List>
+          <Tabs.Trigger value="categories">Categories</Tabs.Trigger>
+          <Tabs.Trigger value="priorities">Priorities</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="categories">
+          {/* <CategorySettings /> */}
+          <p>Categories</p>
+        </Tabs.Content>
+        <Tabs.Content value="priorities">
+          {/* <PrioritySettings /> */}
+          <p>Priorities</p>
+        </Tabs.Content>
+      </Tabs.Root>
     </div>
   );
 }
@@ -23,7 +37,7 @@ function PrioritySettings() {
   return (
     <div>
       <h1>Priority Settings</h1>
-      
+
 
       {priorities.map((priority, index) => (
         <div key={index}>{priority}</div>
@@ -31,5 +45,6 @@ function PrioritySettings() {
     </div>
   );
 }
+
 
 export default Settings;
